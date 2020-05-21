5687e7079554255c6158f9a1359dba6463a0cb31
abidurr <42abid@gmail.com>
2020-05-21 01:28:25 -0400

    addProduct

--- api/controllers/products.ts
+++ api/controllers/products.ts
@@ -1,3 +1,4 @@
+import { v4 } from "https://deno.land/std/uuid/mod.ts"
 import { Product } from "../types.ts"
 
 let products: Product[] = [
@@ -60,8 +61,25 @@ const getProduct = ({ params, response }: { params: { id: string }, response: an
 // @desc    Add a product
 // @route   POST /api/v1/products
 
-const addProduct = ({ response }: { response: any }) => {
-    response.body = "add a product"
+const addProduct = async ({ request, response }: { request: any, response: any }) => {
+    const body = await request.body()
+
+    if (!request.hasBody) {
+        response.status = 400
+        response.body = {
+            success: false,
+            msg: "No data"
+        }
+    } else {
+        const product: Product = body.value
+        product.id = v4.generate()
+        products.push(product)
+        response.status = 201
+        response.body = {
+            success: true,
+            data: product
+        }
+    }
 }
 
 
@@ -70,8 +88,8 @@ const addProduct = ({ response }: { response: any }) => {
 // @desc    Update a product
 // @route   PUT /api/v1/products/:id
 
-const updateProduct = ({ response }: { response: any }) => {
-    response.body = "update"
+const updateProduct = ({ params, request, response }: { params: { id: string }, request: any, response: any }) => {
+    
 }
 
 
