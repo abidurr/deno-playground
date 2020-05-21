dfc6d41363fa04185e6b904701acc641c12beea8
abidurr <42abid@gmail.com>
2020-05-20 21:40:52 -0400

    Route boilerplate

--- api/controllers/products.ts
+++ api/controllers/products.ts
@@ -20,3 +20,59 @@ let products: Product[] = [
         price: 69.99,
     },
 ]
+
+
+// @desc    Get all products
+// @route   GET /api/v1/products
+
+const getProducts = ({ response }: { response: any }) => {
+    response.body = {
+        success: true,
+        data: products
+    }
+}
+
+
+
+
+// @desc    Get a single product
+// @route   GET /api/v1/products/:id
+
+const getProduct = ({ response }: { response: any }) => {
+    response.body = "get single product"
+}
+
+
+
+
+// @desc    Add a product
+// @route   POST /api/v1/products
+
+const addProduct = ({ response }: { response: any }) => {
+    response.body = "add a product"
+}
+
+
+
+
+// @desc    Update a product
+// @route   PUT /api/v1/products/:id
+
+const updateProduct = ({ response }: { response: any }) => {
+    response.body = "update"
+}
+
+
+
+
+// @desc    Delete a product
+// @route   DELETE /api/v1/products/:id
+
+const deleteProduct = ({ response }: { response: any }) => {
+    response.body = "delete"
+}
+
+
+
+
+export { getProducts, getProduct, addProduct, updateProduct, deleteProduct }
